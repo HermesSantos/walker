ffae5a33a96138fd6e17012dfd68c58512c9ec30
Hermes <hermessantos995@gmail.com>
2023-12-01 11:30:46 -0300

    feat: adding others creators and inquirer

--- src/walker.js
+++ src/walker.js
@@ -1,4 +1,6 @@
 import puppeteer from 'puppeteer';
+import path from 'path'
+import inquirer from 'inquirer';
 import { getName } from './helpers/name.js';
 import { generateCnpj } from './helpers/cnpj.js';
 import { getCPF } from './helpers/cpf.js';
@@ -6,7 +8,34 @@ import { generatePhoneNumber } from './helpers/phone.js';
 import { generateEmail } from './helpers/email.js';
 import { makeHost } from './helpers/host.js'
 
-async function walker() {
+
+inquirer.
+  prompt([
+    {
+      name: 'choices',
+      message: 'Criar usuário para: ',
+      type: 'list',
+      choices: ['Parceiro', 'Parceiro Cliente PJ', 'Parceiro Cliente PF', 'Cliente Inbraep PF', 'Cliente Inbraep PJ']
+    }
+  ])
+  .then(answers => {
+    if (answers.choices === 'Parceiro') {
+      walkerPartner()
+    } else if (answers.choices === 'Parceiro Cliente PJ') {
+      walkerClientPartnerPJ()
+    } else if (answers.choices === 'Parceiro Cliente PF') {
+      walkerClientPartnerPF()
+    } else if (answers.choices === 'Cliente Inbraep PF') {
+      walkerInbraepClientPF()
+    } else if (answers.choices === 'Cliente Inbraep PJ') {
+      walkerInbraepClientPJ()
+    }
+  })
+
+const urlInbraep = 'http://localhost:8081'
+const urlparceiro = 'http://localhost:8080'
+
+async function walkerPartner() {
   const data = {
     name: getName(),
     cnpj: generateCnpj(),
@@ -15,7 +44,11 @@ async function walker() {
     email: generateEmail(),
     brand: makeHost()
   }
-  const url = 'http://localhost:8081/cadastro-parceiro'
+  // converte o caminho para um caminho absoluto do arquivo
+  const filePath = path.resolve('sample.pdf')
+
+  const url = `${urlparceiro}/cadastro-parceiro`
+
   console.log('Parceiro criado: ', data)
   const browser = await puppeteer.launch({
     headless: false
@@ -48,9 +81,7 @@ async function walker() {
     page.click('div.dz-default.dz-message')
   ])
 
-  // *************************************************
-  await fileChoser.accept(['C:\\Users\\ichig\\Downloads\\nota_fiscal_03_11.pdf'])
-  // *************************************************
+  await fileChoser.accept([filePath])
 
   await page.type('#cep', '68929516');
   await page.type('#number', '12');
@@ -74,6 +105,176 @@ async function walker() {
   // await browser.close();
 }
 
-// (async () => {
-// })();
-walker()
+async function walkerClientPartnerPJ() {
+  const data = {
+    name: getName(),
+    cnpj: generateCnpj(),
+    cpf: getCPF(),
+    phone: generatePhoneNumber(),
+    email: generateEmail(),
+    brand: makeHost()
+  }
+  // converte o caminho para um caminho absoluto do arquivo
+  const filePath = path.resolve('sample.pdf')
+  console.log(filePath)
+  const url = `${urlparceiro}/register`
+  console.log('Cliente Parceiro PJ criado: ', data)
+  const browser = await puppeteer.launch({
+    headless: false
+  });
+  const page = await browser.newPage();
+
+  await page.goto(url);
+
+  await page.setViewport({ width: 1080, height: 1024 });
+
+  const xpath = `//*[contains(text(), 'Pessoa Jurídica')]`;
+  await page.waitForXPath(xpath)
+  const element = await page.$x(xpath);
+  await element[0].click()
+  await page.waitForSelector('#company');
+  await page.type('#company', data.name);
+  await page.type('#trade_name', data.name);
+  await page.type('#cnpj', data.cnpj);
+  await page.type('#pf-cpf', data.cpf);
+  await page.select('#simple_national', 'Sim');
+  await page.type('#responsible', data.name);
+  await page.type('#phone', data.phone);
+  await page.type('#email_nf', data.email);
+  await page.type('#email_pf', data.email);
+  await page.type('#confirmed-email', data.email);
+  await page.type('#password', '123123');
+  await page.type('#confirmed-pass', '123123');
+  await page.click('span.icon.dropdown-toggle');
+  await page.click('button.btn.bg-blue.pull-right');
+  // descomenta aqui se quiser fechar o browser depois que faz o parceiro
+  // await browser.close();
+}
+
+async function walkerClientPartnerPF() {
+  const data = {
+    name: getName(),
+    cnpj: generateCnpj(),
+    cpf: getCPF(),
+    phone: generatePhoneNumber(),
+    email: generateEmail(),
+    brand: makeHost()
+  }
+  // converte o caminho para um caminho absoluto do arquivo
+  const filePath = path.resolve('sample.pdf')
+  console.log(filePath)
+  const url = `${urlparceiro}/register`
+  console.log('Cliente Parceiro PF criado: ', data)
+  const browser = await puppeteer.launch({
+    headless: false
+  });
+  const page = await browser.newPage();
+
+  await page.goto(url);
+
+  await page.setViewport({ width: 1080, height: 1024 });
+
+  const xpath = `//*[contains(text(), 'Pessoa Física')]`;
+  await page.waitForXPath(xpath)
+  const element = await page.$x(xpath);
+  await element[0].click()
+  await page.waitForSelector('#name');
+  await page.type('#name', data.name);
+  await page.type('#pf-cpf', data.cpf);
+  await page.type('#phone', data.phone);
+  await page.type('#email_pf', data.email);
+  await page.type('#confirmed-email', data.email);
+  await page.type('#password', '123123');
+  await page.type('#confirmed-pass', '123123');
+  await page.click('span.icon.dropdown-toggle');
+  await page.click('button.btn.bg-blue.pull-right');
+  // descomenta aqui se quiser fechar o browser depois que faz o parceiro
+  // await browser.close();
+}
+
+async function walkerInbraepClientPF() {
+  const data = {
+    name: getName(),
+    cnpj: generateCnpj(),
+    cpf: getCPF(),
+    phone: generatePhoneNumber(),
+    email: generateEmail(),
+    brand: makeHost()
+  }
+  // converte o caminho para um caminho absoluto do arquivo
+  const filePath = path.resolve('sample.pdf')
+  console.log(filePath)
+  const url = `${urlInbraep}/register`
+  console.log('Cliente Inbraep PF criado: ', data)
+  const browser = await puppeteer.launch({
+    headless: false
+  });
+  const page = await browser.newPage();
+
+  await page.goto(url);
+
+  await page.setViewport({ width: 1080, height: 1024 });
+
+  const xpath = `//*[contains(text(), 'Pessoa Física')]`;
+  await page.waitForXPath(xpath)
+  const element = await page.$x(xpath);
+  await element[0].click()
+  await page.waitForSelector('#name');
+  await page.type('#name', data.name);
+  await page.type('#pf-cpf', data.cpf);
+  await page.type('#phone', data.phone);
+  await page.type('#email_pf', data.email);
+  await page.type('#confirmed-email', data.email);
+  await page.type('#password', '123123');
+  await page.type('#confirmed-pass', '123123');
+  await page.click('span.icon.dropdown-toggle');
+  await page.click('button.btn.bg-blue.pull-right');
+  // descomenta aqui se quiser fechar o browser depois que faz o parceiro
+  // await browser.close();
+}
+
+async function walkerInbraepClientPJ() {
+  const data = {
+    name: getName(),
+    cnpj: generateCnpj(),
+    cpf: getCPF(),
+    phone: generatePhoneNumber(),
+    email: generateEmail(),
+    brand: makeHost()
+  }
+  // converte o caminho para um caminho absoluto do arquivo
+  const filePath = path.resolve('sample.pdf')
+  console.log(filePath)
+  const url = `${urlInbraep}/register`
+  console.log('Cliente Inbraep PJ criado: ', data)
+  const browser = await puppeteer.launch({
+    headless: false
+  });
+  const page = await browser.newPage();
+
+  await page.goto(url);
+
+  await page.setViewport({ width: 1080, height: 1024 });
+
+  const xpath = `//*[contains(text(), 'Pessoa Jurídica')]`;
+  await page.waitForXPath(xpath)
+  const element = await page.$x(xpath);
+  await element[0].click()
+  await page.waitForSelector('#company');
+  await page.type('#company', data.name);
+  await page.type('#trade_name', data.name);
+  await page.type('#cnpj', data.cnpj);
+  await page.type('#pf-cpf', data.cpf);
+  await page.select('#simple_national', 'Sim');
+  await page.type('#responsible', data.name);
+  await page.type('#phone', data.phone);
+  await page.type('#email_nf', data.email);
+  await page.type('#email_pf', data.email);
+  await page.type('#confirmed-email', data.email);
+  await page.type('#password', '123123');
+  await page.type('#confirmed-pass', '123123');
+  await page.click('span.icon.dropdown-toggle');
+  await page.click('button.btn.bg-blue.pull-right');
+  // descomenta aqui se quiser fechar o browser depois que faz o parceiro
+  // await browser.close();
+}
